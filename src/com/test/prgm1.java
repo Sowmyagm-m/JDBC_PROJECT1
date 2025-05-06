a8c39c310bf30f77fa26055ae8fcc966b5be51a6
Sowmyagm-m <117572569+Sowmyagm-m@users.noreply.github.com>
2025-05-06 09:34:01 +0530

    Update prgm1.java

--- src/com/test/prgm1.java
+++ src/com/test/prgm1.java
@@ -1,9 +1,14 @@
 package com.test;
-
+import java.util.Scanner;
 public class prgm1 {
 
 	public static void main(String[] args) {
 System.out.println("hello world");
-	}
+		Scanner sc=new Scanner(System.in);
+	System.out.println("enter num");
+		int num=sc.nextInt();
+		System.out.println("Square of "+num+"is"+(num*num));
+		
+		}
 
 }
